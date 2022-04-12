952f82c957cffa9527ef1e5fa323bee5c6034df1
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-04-11 20:44:33 -0700

    program: print all 2 digit num combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible different combination
+ * of two digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
