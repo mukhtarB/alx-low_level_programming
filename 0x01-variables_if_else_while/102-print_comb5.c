a53b34a09cac0251f885b3c07dae3a112e937f80
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-04-11 20:59:42 -0700

    program: print all combinations of two 2 digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,17 +9,18 @@
 int main(void)
 {
 	int i, j;
+
 	for (i = 0; i < 100; i++)
 	{
 		for (j = 0; j < 100; j++)
 		{
 			if (i < j)
 			{
-				putchar( (i / 10) + 48 );
-				puthcar( (i % 10) + 48 );
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
-				putchar( (j / 10) + 48 );
-				putchar( (j % 10) + 48 );
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
 
 				if (i != 98 || j != 99)
 				{
