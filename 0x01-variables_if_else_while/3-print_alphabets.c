bd9a97b6419ba53aa51c614cbbd79ff9b6ffe766
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-04-11 17:24:48 -0700

    program: print lower and upcase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - orints the alphabets in lowercase,
+ * and then in uppercase, followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
