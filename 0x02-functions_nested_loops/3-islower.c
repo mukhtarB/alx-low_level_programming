ae5563cb40dfa670d9f863ab0bf945391d191882
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-04-13 03:33:22 -0700

    program: checks char for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - check for lowercase char
+ * @c: char to be checked
+ *
+ * Return: 1 if lowercase, 0 if not
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
