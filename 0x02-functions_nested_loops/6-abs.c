fd4ab9bd8aba51742bd43fa3e8e350c4200b7621
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-04-13 04:15:06 -0700

    program: return the absolute value of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - Computes Absolute Value of integer
+ * @n: the integer value
+ *
+ * Return: the absolute integer value
+ */
+int _abs(int n)
+{
+	return ((n < 0) ? -n : n);
+
+}
