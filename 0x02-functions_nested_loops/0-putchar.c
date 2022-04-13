48ff882fa6e234a68059d961d58d03b225080903
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-04-13 02:45:34 -0700

    program: print _putchar using _putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main- print _putchar, followed by a new line
+ * using only _putchar()
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar(95);
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return 0;
+}
