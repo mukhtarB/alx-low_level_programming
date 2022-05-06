ba04bf34f9b6c35981e732cfb81d255fcbbdf2e2
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-06 10:16:55 -0700

    exec: create executable script to create library

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c
-ar -rc liball *.o
-ar -t liball # list all library object files.
-ranlib liball
+ar -rc liball.a *.o
+ar -t liball.a # list all library object files.
+ranlib liball.a
