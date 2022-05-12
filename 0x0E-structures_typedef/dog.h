d0d693280d65e87d77a2104e02c27fb5e29e7e61
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-12 16:12:32 -0700

    create struct type alias

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @owner: String pointer to the owner variable.
  * @age: Float variable for dog age.
 */
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
