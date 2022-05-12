2f5e5224c63f5ff0d25da62f1e052ec9a20b747e
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-12 15:17:21 -0700

    task 0: create struct for header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Structure holding the properties of a dog.
+ *
+ * @name: String pointer to the name variable.
+ * @owner: String pointer to the owner variable.
+ * @age: Float variable for dog age.
+*/
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif /* ifndef _DOG_H_ */
