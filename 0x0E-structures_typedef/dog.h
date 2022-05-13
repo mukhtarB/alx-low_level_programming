40eb2f240341c6ce91fd6656ef28fb298875e69c
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-13 10:44:29 -0700

    update header with func prototype, task 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,6 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* ifndef _DOG_H_ */
