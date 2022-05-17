9f712e0ebc3fff23e2030a785e2d6feca1f405af
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-16 21:28:46 -0700

    prorgam: print no of args passed via cmd

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print number of arguements passed via cmd
+ * @argc: Number of args passed via cmd
+ * @argv: List of args passed via cmd
+ *
+ * Return: Always (0), Success
+ */
+int main(int argc, char __attribute__((unused)) **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
