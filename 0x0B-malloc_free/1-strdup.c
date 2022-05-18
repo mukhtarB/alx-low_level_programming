7d0f82c24beca1a6c4c14b931ec342178a53df38
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-18 02:34:35 -0700

    program: duplicate str content

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include "main.h"
+
+char *_strcopy(char *newstr, char *str);
+int _strlen(char *str);
+
+
+/**
+ * _strdup - return new pointer to duplicate of  string
+ * @str: - pointer to exisiting string
+ *
+ * Return: Pointer || NULL
+ */
+char *_strdup(char *str)
+{
+	int len = _strlen(str);
+	char *newerstr;
+
+	char *allocated_ptr = (char *) malloc((len + 1) * sizeof(*str));
+
+	if (str == NULL || allocated_ptr == NULL)
+	{
+		free(allocated_ptr);
+		return (NULL);
+	}
+
+	newerstr = _strcopy(allocated_ptr, str);
+
+	return (newerstr);
+}
+
+
+/**
+ * _strcopy - Copy string1 into string2
+ * @newstr: string2, the new string
+ * @str: string1, the older string
+ *
+ * Return: pointer to new string
+ */
+char *_strcopy(char *newstr, char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		newstr[i] = str[i];
+
+	newstr[i] = '\0';
+
+	return (newstr);
+}
+
+
+/**
+ * _strlen - calculate the length of a string
+ * @str: pointer to string
+ *
+ * Return: length of string
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	return (len);
+}
