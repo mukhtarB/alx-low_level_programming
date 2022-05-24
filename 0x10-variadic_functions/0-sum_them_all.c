2a07ef616d4c43cfb14e4e75a53a336ae774909b
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-24 06:11:34 -0700

    program: add up all variable arguements of program

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function that sums all int arguements given
+ * @n: no of integers to sum
+ *
+ * Returns: an integer representing sum of numbers of n
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list arg_ptr;
+
+	unsigned int i, sum = 0;
+
+	va_start(arg_ptr, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(arg_ptr, int);
+
+	va_end(arg_ptr);
+	return (sum);
+}
