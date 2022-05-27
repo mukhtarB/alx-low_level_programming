732ad364e3e226817fc192347858e134b2c7acbc
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-27 13:41:32 -0700

    program: add all int cmd args

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 		j = 0;
 		while (argv[i][j] != '\0')
 		{
-			if (argv[i] < 48 || argv[i] > 57)
+			if (argv[i][j] < 48 || argv[i][j] > 57)
 			{
 				printf("Error\n");
 				return (1);
