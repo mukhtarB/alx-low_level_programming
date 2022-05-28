86f0141a96c9d01750ae2e4946094566932c3afc
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-28 10:16:19 -0700

    program: creates a new dog from struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,22 +12,72 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	/* Init new_dog var */
-	/* declare var of typr dog struct */
 	dog_t *my_new_dog;
+	char *name_ptr, *owner_ptr;
 
-	/* dynamicaaly allocate memory space for said variable */
+	if (!name || age < 0 || !owner)
+		return (NULL);
+
 	my_new_dog = malloc(sizeof(dog_t));
+	name_ptr = malloc(sizeof(char) * (_len(name) + 1));
+	owner_ptr = malloc(sizeof(char) * (_len(owner) + 1));
 
-	if (my_new_dog == NULL)
+
+	if (!my_new_dog || !name_ptr || !owner_ptr)
 	{
 		free(my_new_dog);
+		free(name_ptr);
+		free(owner_ptr);
 		return (NULL);
 	}
 
-	my_new_dog->name = name;
+	/* return pointer to allocated string to struct str(s) */
+	my_new_dog->name = _strcpy(name_ptr, name);
 	my_new_dog->age = age;
-	my_new_dog->owner = owner;
+	my_new_dog->owner = _strcpy(owner_ptr, owner);
 
 	return (my_new_dog);
+}
+
+
+/**
+ * _len - Finds the length of a string
+ * @str: The string
+ *
+ * Return: an int number repr the length of str
+ */
+
+int _len(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
+}
+
+
+/**
+ * _strcpy - Copy a string from s1 to s2 in memory
+ * @s2: new str
+ * @s1: old str
+ *
+ * Return: pointer to new str (s2)
+ */
+
+char *_strcpy(char *s2, char *s1)
+{
+	int i = 0;
+
+	while (*(s1 + 1) != '\0')
+	{
+		/* pointer arithmetic */
+		*(s2 + 1) = *(s1 + 1);
+		i++;
+	}
+
+	*(s2 + 1) = '\0';
+
+	return (s2);
 }
