52424a6cc8ed00b39b1b6a25c5bca71b81481b6b
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-28 10:15:39 -0700

    updated header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,8 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+int _len(char *str);
+char *_strcpy(char *s2, char *s1);
 dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* ifndef _DOG_H_ */
