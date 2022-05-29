d52df7154164a94f858ec1b5ac7fb8dd790d82f1
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-28 17:11:55 -0700

    updated hader file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,5 +5,7 @@ int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int _strlen(char *str);
+char *_strappend(char *newstr, char *str, unsigned int index);
 
 #endif /* MAIN_H */
