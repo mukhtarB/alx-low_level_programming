33d7b64c5441e4e40c810b4a8122b0fa99056a4e
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-29 13:06:00 -0700

    updated header file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,5 +8,6 @@ char *str_concat(char *s1, char *s2);
 int _strlen(char *str);
 char *_strappend(char *newstr, char *str, unsigned int index);
 int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 
 #endif /* MAIN_H */
