315292c5bbe0777d061688a3760bd5c56b4d6606
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-29 13:06:56 -0700

    program: free malloc from 2d arr matrix

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - Matrix grid to clear memory on
+ * @height: size of outer array and height of matrix
+ */
+
+void free_grid(int **grid, int height)
+{
+	int ndx_i;
+
+	for (ndx_i = 0; ndx_i < height; ndx_i++)
+	{
+		free(grid[ndx_i]);
+	}
+	free(grid);
+}
