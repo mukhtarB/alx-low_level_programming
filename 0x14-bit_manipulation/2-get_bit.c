ba1b24e58d174c60dc0be5d81c1492894311f193
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-30 19:15:35 -0700

    setup program file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,2 +1,16 @@
 #include "main.h"
 
+/**
+ * get_bit - Returns value of bit at given index
+ * @n: integer number
+ * @index: given index
+ *
+ * Return: Bit Values (Success) || -1 (Error)
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	(void)(n);
+	(void)(index);
+        return (-1);
+}
