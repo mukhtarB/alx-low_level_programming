3435e4f17a4ffb34a8344417b1f174997bb765e5
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-30 18:59:07 -0700

    setup: program file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,1 +1,13 @@
 #include "main.h"
+
+/**
+ * print_binary - Convert innteger to binary
+ * @n: integer number
+ *
+ * Return: Binary Representation (Success) || 0 (Error)
+ */
+
+void print_binary(unsigned long int n);
+{
+
+}
