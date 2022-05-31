b73ed0ae8e96dd86f892361e7d231689ff8086dc
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-30 19:35:05 -0700

    program: set bit to 0, at index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,2 +1,16 @@
 #include "main.h"
 
+/**
+ * clear_bit - set value of bit to 1 at given index
+ * @n: pointer to the integer
+ * @index: given index
+ *
+ * Return: 1 (Success) || -1 (Error)
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	(void)(n);
+	(void)(index);
+	return (-1);
+}
