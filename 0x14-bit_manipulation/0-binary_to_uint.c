6cb54f9f89533fbd6b9d4baca247c917c9502528
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-30 18:48:49 -0700

    program: convert binary to integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,4 +9,19 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int ndx_i = 0, dec = 0;
+
+	if (!b)
+		return (0);
+
+	while (b[ndx_i] != '\0')
+	{
+		if (b[ndx_i] < '0' || b[ndx_i] > '1')
+			return (0);
+
+		dec = 2 * dec + (b[ndx_i] - '0');
+		ndx_i++;
+	}
+
+	return (dec);
 }
