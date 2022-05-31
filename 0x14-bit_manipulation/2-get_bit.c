48201ee6efc1b49a0c0288b30cc9652f709b2bd8
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-30 19:20:17 -0700

    program: get vit value at index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,12 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	(void)(n);
-	(void)(index);
-        return (-1);
+	int bit;
+
+	if (index > 63)
+		return (-1);
+
+	bit = (n >> index) & 1;
+
+	return (bit);
 }
