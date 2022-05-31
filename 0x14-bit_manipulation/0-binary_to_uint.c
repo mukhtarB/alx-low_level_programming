46777905841b7fa01c116ee3af028daf88ff593c
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-30 18:13:07 -0700

    program: convert binary to integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,12 @@
 #include "main.h"
+
+/**
+ * binary_to_uint - Converts binary to unsigned int
+ * @b: pointer to char
+ *
+ * Return: converted number (Success) || 0 (Error)
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+}
