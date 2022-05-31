01804fbde2b05a1568dbdd1ef9d8b2318609e8f6
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-05-30 19:43:41 -0700

    program: find no of bits needed to flip numbers

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,2 +1,16 @@
 #include "main.h"
 
+/**
+ * flip_bits - Find no of biys needed to flip number to another
+ * @n: first number
+ * @m: second number
+ *
+ * Return: No of bits needed to flip a number (Success) || 0 (Error)
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	(void)(n);
+	(void)(m);
+	return (0);
+}
