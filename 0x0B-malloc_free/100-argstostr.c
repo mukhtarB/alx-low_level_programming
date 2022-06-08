45d1b4cd511f42da2460332c226203b35407d874
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-06-08 08:33:53 -0700

    program: update argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -15,9 +15,7 @@ char *argstostr(int ac, char *av[])
 
 	if (ac == 0 || !av)
 		return (NULL);
-
 	ndx_i = ndx_j = ndx_k = str_size = 0;
-
 	while (ndx_i < ac)
 	{
 		ndx_j = 0;
@@ -28,9 +26,7 @@ char *argstostr(int ac, char *av[])
 		}
 		ndx_i++;
 	}
-
 	str_size += ndx_i;
-
 	ptr = malloc(sizeof(char) * (str_size + 1));
 	if (!ptr)
 		return (NULL);
@@ -45,11 +41,9 @@ char *argstostr(int ac, char *av[])
 			ndx_k++;
 			ndx_j++;
 		}
-
 		ptr[ndx_k++] = '\n';
 		ndx_i++;
 	}
-
 	ptr[ndx_k] = '\0';
 	return (ptr);
 
