72b6ac4e620332441852382d95ad775c762031d5
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-06-16 18:18:12 -0700

    update header"

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,8 +7,5 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
-void errors(void);
-int _strlen(char *s);
-int is_digit(char *s);
 
 #endif /* MAIN_H */
