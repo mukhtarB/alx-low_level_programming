e0c277c5fedcf2ef305490b7bc2c637e6bd1d4d9
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-07-12 18:57:30 -0700

    program: print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,5 +9,25 @@
 
 void print_binary(unsigned long int n)
 {
-	_putchar(n);
+	int i, count = 0;
+	unsigned long int current;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+		{
+			_putchar('0');
+		}
+	}
+	if (!count)
+	{
+		_putchar('0');
+	}
 }
